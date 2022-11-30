8712f4f5e07a2e84fbd552f5d31442e657d318ba
Henri Immonen <henri.immonen000@gmail.com>
2022-11-30 09:21:24 +0200

    some corrections to names and send forms button

--- src/routes.py
+++ src/routes.py
@@ -7,6 +7,6 @@ def index():
     start_date = functions.get_start_date()
     return render_template('index.html', start_date=start_date)
 
-@app.route('/send')
+@app.route('/edit')
 def send_reference():
     return render_template('send_reference.html')
