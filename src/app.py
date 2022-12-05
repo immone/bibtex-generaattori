01d18b6e0d20150f71ae7995b903a446433b8e26
Henri Immonen <henri.immonen000@gmail.com>
2022-12-05 17:34:27 +0200

    Changed back to simple form

--- src/app.py
+++ src/app.py
@@ -2,15 +2,6 @@
 from init import app
 import routes  # pylint: disable=unused-import
 
-class App: # pylint: disable=too-few-public-methods
-    """Main class of the app"""
-    def __init__(self) -> None:
-        pass
-
-    def main(self):
-        """Start the app"""
-        app.run()
 
 if __name__ == '__main__':
-    app = App()
-    app.main()
+    app.run()
