7c8b84e1fa6ffd97b1f30ec6ad877cc027dfdea2
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-12-07 13:49:59 +0200

    Add removing references functionality

--- src/routes.py
+++ src/routes.py
@@ -5,29 +5,29 @@ from services import Services
 
 service = Services()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     """Page for viewing all references."""
-    citations = service.get_db_contents()
-    return render_template(
-        'check_references.html',
-        count=len(citations),
-        citations=citations
-    )
-
-@app.route('/type')
-def choose_reference_type():
-    """Page for choosing refence type."""
-    return render_template('choose_reference_type.html')
+    if request.method == 'GET':
+        citations = service.get_db_contents()
+        return render_template(
+            'check_references.html',
+            count=len(citations),
+            citations=citations
+        )
+    else:
+        ref_id = int(request.form["id"])
+        service.remove_from_db(ref_id)
+        return redirect('/')
 
 @app.route('/edit', methods=['GET', 'POST'])
 def send_reference():
     """New reference page."""
     if request.method == 'GET':
         return render_template('send_reference.html')
-
-    author = request.form['author']
-    title = request.form['title']
-    year = request.form['year']
-    service.save_to_db(author, title, year)
-    return redirect('/')
+    else:
+        author = request.form['author']
+        title = request.form['title']
+        year = request.form['year']
+        service.save_to_db(author, title, year)
+        return redirect('/')
